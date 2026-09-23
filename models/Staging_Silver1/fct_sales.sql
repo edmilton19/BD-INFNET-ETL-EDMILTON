717a26e614b49ec377c416bb366b1e71c61e488f
Jose Edmilton dos Santos <jose.jesus@al.infnet.edu.br>
2026-09-23 18:59:58 +0000

    teste

--- models/Staging_Silver1/fct_sales.sql
+++ models/Staging_Silver1/fct_sales.sql
@@ -1,2 +1,39 @@
-SELECT *
-FROM {{ source('consumo', 'dim_cliente') }}
+WITH pedidos AS (
+    SELECT 
+        id_pedido,
+        data_pedido,
+        id_cliente,
+        canal_venda,
+        endereco_entrega
+    FROM {{ source('consumo', 'pedidos_desnormalizados') }}
+),
+
+vendas AS (
+    SELECT 
+        sk_venda,
+        id_pedido,
+        id_produto,
+        quantidade,
+        preco_unitario,
+        custo_unitario,
+        (quantidade * preco_unitario) AS receita_bruta,
+        (quantidade * custo_unitario) AS custo_total,
+        ((quantidade * preco_unitario) - (quantidade * custo_unitario)) AS lucro_bruto
+    FROM {{ source('consumo', 'fato_vendas') }}
+)
+
+SELECT 
+    v.sk_venda,
+    v.id_pedido,
+    p.data_pedido,
+    p.id_cliente,
+    v.id_produto,
+    p.canal_venda,
+    v.quantidade,
+    v.preco_unitario,
+    v.custo_unitario,
+    v.receita_bruta,
+    v.custo_total,
+    v.lucro_bruto
+FROM vendas v
+LEFT JOIN pedidos p ON v.id_pedido = p.id_pedido
