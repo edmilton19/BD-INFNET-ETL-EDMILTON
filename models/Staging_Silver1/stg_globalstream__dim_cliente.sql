b97001e9490d2d885cd162a0cc8b6f40b170c8b4
Jose Edmilton dos Santos <jose.jesus@al.infnet.edu.br>
2026-09-24 12:34:28 +0000

    limpeza e teste

--- models/Staging_Silver1/stg_globalstream__dim_cliente.sql
+++ models/Staging_Silver1/stg_globalstream__dim_cliente.sql
@@ -1,2 +1,8 @@
-SELECT *
+SELECT
+    CAST(sk_cliente AS STRING) AS sk_cliente,
+    CAST(id_cliente_bk AS STRING) AS id_cliente_bk,
+    UPPER(TRIM(nome_cliente)) AS nome_cliente,
+    LOWER(TRIM(email)) AS email_cliente,
+    UPPER(TRIM(cidade)) AS cidade_cliente,
+    UPPER(TRIM(estado)) AS estado_cliente
 FROM {{ source('consumo', 'dim_cliente') }}
