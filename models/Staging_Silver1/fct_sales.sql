e7a5883bc08a7f498aefefcc9bdddecb6e3eb7bb
Jose Edmilton dos Santos <jose.jesus@al.infnet.edu.br>
2026-09-24 12:37:26 +0000

    ajustes de limpeza

--- models/Staging_Silver1/fct_sales.sql
+++ models/Staging_Silver1/fct_sales.sql
@@ -1,21 +1,21 @@
 WITH pedidos AS (
     SELECT 
-        id_pedido,
-        data_pedido,
-        id_cliente,
-        canal_venda,
+        CAST(id_pedido AS STRING) AS id_pedido,
+        CAST(data_pedido AS DATE) AS data_pedido,
+        CAST(id_cliente AS STRING) AS id_cliente,
+        UPPER(TRIM(canal_venda)) AS canal_venda,
         endereco_entrega
     FROM {{ source('consumo', 'pedidos_desnormalizados') }}
 ),
 
 vendas AS (
     SELECT 
-        sk_venda,
-        id_pedido_bk AS id_pedido,
-        sk_produto AS id_produto,
-        quantidade,
-        preco_unitario,
-        (quantidade * preco_unitario) AS receita_bruta
+        CAST(sk_venda AS STRING) AS sk_venda,
+        CAST(id_pedido_bk AS STRING) AS id_pedido,
+        CAST(sk_produto AS STRING) AS id_produto,
+        CAST(quantidade AS INT64) AS quantidade,
+        CAST(preco_unitario AS NUMERIC) AS preco_unitario,
+        ROUND(CAST(quantidade * preco_unitario AS NUMERIC), 2) AS receita_bruta
     FROM {{ source('consumo', 'fato_vendas') }}
 )
 
